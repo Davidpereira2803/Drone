6c134e8464c1a995d61c1bf18d8a979eb1647fe3
Davidpereira2803 <david.pereira.002@student.uni.lu>
2024-10-10 15:00:36 +0200

    update

---
+++ drone_controls/main.py
@@ -0,0 +1,84 @@
+import tensorflow
+from tensorflow import keras
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import os
+import cv2
+import numpy as np
+from djitellopy import Tello
+from drone_video import *
+import csv
+
+face_classifier = cv2.CascadeClassifier(r'/home/david/Projects/Drone/facialemotionrecognizerinrealtime/haarcascade_frontalface_default.xml')
+classifier =load_model(r'/home/david/Projects/Drone/facialemotionrecognizerinrealtime/model.h5')
+
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+tello = Tello()
+
+def main():
+    print("To start the video feed from the drone press 1.")
+
+    number = input()
+
+    if number == "1": 
+        print("Connecting to the drone!")
+        connect_to_drone(tello)
+        print("Turning stream feed on!")
+        turn_streamon(tello)
+        print("Starting emotion recognition!")
+        start_emotion_cv2()
+
+def start_emotion_cv2():
+    while True: 
+        frame = tello.get_frame_read().frame
+        labels = []
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
+
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                label_position = (x,y)
+                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                append_to_csv('/home/david/Projects/Drone/drone_controls/output.csv', label)
+            else:
+                cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        cv2.imshow('Emotion Detector',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    turn_streamoff(tello)
+    cv2.destroyAllWindows()
+
+def append_to_csv(filename, input_string):
+    """
+    Appends a string to a new row in a CSV file.
+
+    :param filename: The name of the CSV file to append to.
+    :param input_string: A string to be written as a single row.
+    """
+    try:
+        # Open the file in append mode
+        with open(filename, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            # Write the input string as a single row in the CSV file
+            writer.writerow([input_string])
+
+        print(f"'{input_string}' successfully added to {filename}")
+    except Exception as e:
+        print(f"An error occurred while writing to the file: {e}")
+
+if __name__ == "__main__":
+    main()
